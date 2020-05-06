a00fd495599ef49ea40fe4af356b6a314b0e5e2e
Renato Dinhani <renatodinhani@gmail.com>
2020-05-06 04:08:30 -0300

    Fix #7 - Indentation with tabs not respecting end column

--- src/renderers/indentationRenderer.ts
+++ src/renderers/indentationRenderer.ts
@@ -1,9 +1,24 @@
 export class IndentationRenderer {
 
-    render(identationText: string): string {
-        return identationText
+    render(indentationText: string, editorTabSize: number): IndentationRendered {
+        const text = indentationText
             .split("")
             .map(c => c === "\t" ? "\t" : " ")
             .join("");
+
+        const whitespaceWidth = text
+            .split("")
+            .map(c => c === "\t" ? editorTabSize : 1)
+            .reduce((total, n) => total + n, 0);
+
+        return {
+            text: text,
+            whitespaceWidth: whitespaceWidth
+        }
     }
+}
+
+export class IndentationRendered {
+    text: string;
+    whitespaceWidth: number;
 }
